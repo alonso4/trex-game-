1694063e58f6d67978b1f70da849db48fe0fb535
alonso4 <102317605+alonso4@users.noreply.github.com>
2022-04-21 10:03:37 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -73,7 +73,7 @@ function draw(){
   if(ground.x<0){
     ground.x=ground.width/2;
       }
-      if(touches.length<0||keyDown("space")&&trex.y>3*height/7){
+      if((touches.length<0||keyDown("space"))&&trex.y>3*height/7){
         trex.velocityY = -10;
         jumpSound.play();
         touches=[];
